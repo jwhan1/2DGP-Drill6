c1b7539efb2dd14c960209b1f6b2d1b6685ef9c5
jwhan1 <96102340+jwhan1@users.noreply.github.com>
2024-10-02 23:45:22 +0900

    제자리달리기

--- move_my_character_to_hand.py
+++ move_my_character_to_hand.py
@@ -8,19 +8,23 @@ open_canvas()
 
 hand_arrow = load_image('hand_arrow.png')
 character = load_image('animation_sheet.png')
-
 background = load_image('TUK_GROUND.png')
 
 def move_character():
+
     pass
+
 def draw_character():
+    character.clip_draw(frame*100,100,100,100,x,y,100,100)
     pass
+
 running = True
 move = False
 x = width // 2
 y = height // 2
-handX = random.randrange(0, width)
-frame = random.randrange(0, height)
+handX = random.randrange(25, width - 25)
+handY = random.randrange(26, height - 26)
+frame = 0
 while running:
     clear_canvas()
     background.draw(width // 2, height // 2)
@@ -28,7 +32,7 @@ while running:
     draw_character()#캐릭터 그리기
     frame = (frame + 1) % 8
     update_canvas()
-    delay(0.1)
+    delay(0.05)
 
 
 close_canvas()
