78846c73d6b0b1cbc0463a2e99d4fa456539f687
jwhan1 <96102340+jwhan1@users.noreply.github.com>
2024-10-03 00:35:13 +0900

    손을 쫒는 사람

--- move_my_character_to_hand.py
+++ move_my_character_to_hand.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import random
+import math
 width = 800
 height = 600
 
@@ -11,17 +12,31 @@ character = load_image('animation_sheet.png')
 background = load_image('TUK_GROUND.png')
 
 def move_character():
-    if(x== handX and y == handY):
+    global x, y, t
+    if (x == handX and y == handY):
         move_hand()
-    pass
+    if abs(x - handX) < 2:
+        t = 0
+    else:
+        t =  1 - t / (math.sqrt(width ** 2 + height ** 2))**(2/3)
+
+    x = (1- t) * handX + t * x
+    y = (1- t) * handY + t * y
+
 def move_hand():
-        pass
+    global handX, handY
+
+    handX = random.randrange(25, width - 25)
+    handY = random.randrange(26, height - 26)
+
+    move = 0
 running = True
-move = False
-x = width // 2
-y = height // 2
-handX = random.randrange(25, width - 25)
-handY = random.randrange(26, height - 26)
+
+x = width // 2.0
+y = height // 2.0
+t = 0.1;
+handX = x
+handY = y
 frame = 0
 while running:
     clear_canvas()
@@ -32,7 +47,7 @@ while running:
     character.clip_draw(frame * 100, 100, 100, 100, x, y, 100, 100)
     frame = (frame + 1) % 8
     update_canvas()
-    delay(0.05)
+    delay(0.001)
 
 
 close_canvas()
